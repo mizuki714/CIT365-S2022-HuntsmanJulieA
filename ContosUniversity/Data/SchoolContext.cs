f9f9a77c381eb4a9dceb658e6c0d5769f21a9d81
Julie Huntsman <mizuki714@hotmail.com>
2022-06-21 14:37:38 -0500

    work please

---
+++ ContosUniversity/Data/SchoolContext.cs
@@ -0,0 +1,41 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+namespace ContosoUniversity.Data
+{
+    //create a DBSet porperty for each entity
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+        /*
+         * can omit the following statements bc they are implicit
+         student references enrollment, and enrollment references course
+        */
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+        public DbSet<Person> People { get; set; }
+
+        //override defualt behavior so table names arent plural
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Person");
+            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Instructor>().ToTable("Person");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+            modelBuilder.Entity<Person>().ToTable("Person");
+
+            modelBuilder.Entity<CourseAssignment>()
+                .HasKey(c => new { c.CourseID, c.InstructorID });
+        }
+    }
+}
+
---
+++ ContosUniversity/Models/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+    //enrollment one enrollment to one student and one enrollment per course
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }//pk
+        public int CourseID { get; set; }//fk
+        public int StudentID { get; set; }//fk
+
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grade? Grade { get; set; }//enum can be NUll 
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+
+    }
+}
